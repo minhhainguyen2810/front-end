cbc2077407357d11efde4b594bbe7964e7783a93
minhnguyen1305 <minhhainguyen+1@kms-technology.com>
2019-09-03 10:43:42 +0700

    :v:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My App</h1>
+      <img class="App-logo" src="images/Image from iOS.png" />
+    </div>
+  )
+}
+
+export default App
