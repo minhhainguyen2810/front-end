aacabb63cff001612a6e5f6f6303c6520d830946
minhhainguyen2810 <champion.minh@gmail.com>
2020-01-04 17:42:35 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import configureStore from './configureStore'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const store = configureStore()
-process.env.NODE_ENV === 'production' &&
-  console.log('%cLàm gì vậy, tính hách hệ thống người ta à', 'color: pink; font-size: 50px;')
+console.log('%cLàm gì vậy, tính hách hệ thống người ta à', 'color: pink; font-size: 50px;')
 
 render(
   <Provider store={store}>
